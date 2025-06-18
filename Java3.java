39fd1bc14887fcfc8655e482934d640d792deb0f
HansaRasoga <hansarasoga@gmail.com>
2025-06-18 14:53:43 +0530

    1st editing

--- Java3.java
+++ Java3.java
@@ -10,10 +10,19 @@ class Dog extends Animal {
     }
 }
 
+class Cat extends Animal {
+    void meow() {
+        System.out.println("The cat meows.");
+    }
+}
+
 public class Java3 {
     public static void main(String[] args) {
         Dog myDog = new Dog();
         myDog.eat();   // Inherited method
         myDog.bark();  // Dog's own method
+        Cat myCat = new Cat();
+        myCat.eat();   // Inherited method
+        myCat.meow();  // Cat's own method
     }
 }
